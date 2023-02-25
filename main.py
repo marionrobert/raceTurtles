275047c49089540db67cf36c6b15e619f4a1fb9a
Marion <mrobert148@gmail.com>
2023-02-25 10:23:39 +0100

    tests with method listen on screen

--- main.py
+++ main.py
@@ -6,6 +6,34 @@ y_positions = [-70, -40, -10, 20, 50, 80]
 screen = Screen()
 screen.setup(width=500, height=450)
 
+# def move_forwards():
+#     tim.penup()
+#     tim.forward(10)
+#
+#
+# def move_backwards():
+#     tim.penup()
+#     tim.backward(10)
+#
+#
+# def turn_left():
+#     tim.left(10)
+#
+#
+# def turn_right():
+#     tim.right(10)
+#
+# def clear():
+#     tim.clear()
+#     tim.home()
+#
+#
+# screen.listen()
+# screen.onkey(fun=move_forwards, key="w")
+# screen.onkey(fun=move_backwards, key="s")
+# screen.onkey(fun=turn_left, key="a")
+# screen.onkey(fun=turn_right, key="d")
+# screen.onkey(fun=clear, key="c")
 screen.exitonclick()
 
 
