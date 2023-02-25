797bd5e9507829214c58ccf5afaa0335f922eeef
Marion <mrobert148@gmail.com>
2023-02-25 10:20:29 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+colors = ["red", "green", "blue", "orange", "pink", "yellow"]
+turtles = []
+y_positions = [-70, -40, -10, 20, 50, 80]
+screen = Screen()
+screen.setup(width=500, height=450)
+
+screen.exitonclick()
+
+
