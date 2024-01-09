35ea73042a4dbcc8e49e4a784a62ebe74bc86798
Marion Robert <107509668+marionrobert@users.noreply.github.com>
2024-01-09 16:59:05 -0500

    code commenté + affichage du résultat directeemnt dans l'interface d ejeu

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from turtle import Turtle, Screen
 import random
 
+#  initialisation du style
+ALIGNMENT = "center"
+FONT = ("Courier", 15, "bold")
+
+# initialisation des variables nécessaires à la préparation du jeu
 colors = ["red", "green", "blue", "orange", "pink", "yellow"]
 turtles = []
 y_positions = [-70, -40, -10, 20, 50, 80]
-screen = Screen()
-screen.setup(width=500, height=450)
 
+# création et configuration de l'interface 
+screen = Screen()
+screen.setup(width=550, height=450)
+screen.title("Turtles race")
+
+# création du tableau des scores
+scoreboard = Turtle()
+scoreboard.hideturtle()
+scoreboard.penup()
+scoreboard.goto(0, 120)
+
+# création des tortues et positionnement sur la ligne de départ
 for n in range(6):
     new_turtle = Turtle(shape="turtle")
     new_turtle.color(colors[n])
@@ -14,6 +29,7 @@ for n in range(6):
     new_turtle.goto(-230, y_positions[n])
     turtles.append(new_turtle)
 
+# création du prompt de récupération du pari du joueur
 message = ""
 for color in colors:
     if color != colors[-1]:
@@ -32,38 +48,11 @@ while game_on:
         if turtle.xcor() > 215:
             game_on = False
             if turtle.pencolor() == user_bet:
-                print(f"You win, the turtle {turtle.color()[0]} is the winner!")
+                # print(f"You win, the turtle {turtle.color()[0]} is the winner!")
+                scoreboard.write(f"You win, the turtle {turtle.color()[0]} is the winner!", align=ALIGNMENT, font=FONT)
             else:
-                print(f"You loose, the turtle {turtle.color()[0]} is the winner!")
+                # print(f"You loose, the turtle {turtle.color()[0]} is the winner!")
+                scoreboard.write(f"You loose, the turtle {turtle.color()[0]} is the winner!", align=ALIGNMENT, font=FONT)
 
-# def move_forwards():
-#     tim.penup()
-#     tim.forward(10)
-#
-#
-# def move_backwards():
-#     tim.penup()
-#     tim.backward(10)
-#
-#
-# def turn_left():
-#     tim.left(10)
-#
-#
-# def turn_right():
-#     tim.right(10)
-#
-# def clear():
-#     tim.clear()
-#     tim.home()
-#
-#
-# screen.listen()
-# screen.onkey(fun=move_forwards, key="w")
-# screen.onkey(fun=move_backwards, key="s")
-# screen.onkey(fun=turn_left, key="a")
-# screen.onkey(fun=turn_right, key="d")
-# screen.onkey(fun=clear, key="c")
+
 screen.exitonclick()
-
-
